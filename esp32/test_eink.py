7484cafa1097d1879c144e07380aa2964461398a
CbGames09 <141709496+CbGames09@users.noreply.github.com>
2025-10-25 20:16:25 +0200

    Update test_eink.py

--- esp32/test_eink.py
+++ esp32/test_eink.py
@@ -2,22 +2,27 @@ from machine import Pin, SPI
 from epaper1in54b import EPD
 import time
 
-spi = SPI(2, baudrate=4000000, polarity=0, phase=0,
+spi = SPI(1, baudrate=4000000, polarity=0, phase=0,
           sck=Pin(48), mosi=Pin(38))
-eink = EPD(spi, Pin(21), Pin(18), Pin(14), Pin(46))
+
+cs = Pin(21)
+dc = Pin(18)
+rst = Pin(14)
+busy = Pin(8, Pin.IN)
+
+print("Init display ...")
+eink = EPD(spi, cs, dc, rst, busy)
 eink.init()
 
 fb_black = bytearray(5000)
 fb_red = bytearray(5000)
 
-# Schwarz füllen
 for i in range(5000):
-    fb_black[i] = 0x00
+    fb_black[i] = 0x00  # schwarz
     fb_red[i] = 0x00
 
-print("Refreshing...")
+print("Display refresh ...")
 eink.display_frame(fb_black, fb_red)
-print("Done.")
-
+print("Done")
 time.sleep(10)
 eink.sleep()
